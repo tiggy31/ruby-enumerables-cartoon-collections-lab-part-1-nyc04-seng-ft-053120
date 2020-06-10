41221e1b444c5f7a9e069e9f1f75e1087befb468
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 20:07:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
       require "pry"
 def greet_characters(array)
-  array.each do |element|
-   puts "Hello #{element}!"
+  array.each { |element| puts "Hello #{element}!" }"
 end
 
 def list_dwarves(array)
