cc18429876f3466d8f2e3684a11ba09244ee4065
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 19:57:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,7 @@
 def greet_characters(array)
   
   array.each do |characters_array|
-    puts "Hello, " #{array.characters_array+ "!"
-  
+  puts "Hello, #{array.characters_array}!"
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
